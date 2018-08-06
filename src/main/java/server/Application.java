0fb20ff98a068c521803165c87af67e8dafedfc4
Yuhan Niu <yuhan.niu@token.io>
2018-08-06 17:25:56 +0100

    Improve Error Handling

--- src/main/java/server/Application.java
+++ src/main/java/server/Application.java
@@ -1,5 +1,17 @@
 package server;
 
+import static io.grpc.Status.Code.ALREADY_EXISTS;
+import static io.grpc.Status.Code.DEADLINE_EXCEEDED;
+import static io.grpc.Status.Code.FAILED_PRECONDITION;
+import static io.grpc.Status.Code.INTERNAL;
+import static io.grpc.Status.Code.INVALID_ARGUMENT;
+import static io.grpc.Status.Code.NOT_FOUND;
+import static io.grpc.Status.Code.OUT_OF_RANGE;
+import static io.grpc.Status.Code.PERMISSION_DENIED;
+import static io.grpc.Status.Code.RESOURCE_EXHAUSTED;
+import static io.grpc.Status.Code.UNAUTHENTICATED;
+import static io.grpc.Status.Code.UNAVAILABLE;
+import static io.grpc.Status.Code.UNIMPLEMENTED;
 import static spark.Spark.delete;
 import static spark.Spark.exception;
 import static spark.Spark.get;
@@ -14,15 +26,21 @@ import com.typesafe.config.Config;
 import com.typesafe.config.ConfigFactory;
 import grpcbridge.Bridge;
 import grpcbridge.BridgeBuilder;
-import grpcbridge.Exceptions.BridgeException;
 import grpcbridge.http.HttpMethod;
 import grpcbridge.http.HttpRequest;
 import io.grpc.Server;
 import io.grpc.ServerBuilder;
+import io.grpc.Status;
+import io.grpc.StatusRuntimeException;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import spark.Request;
+import spark.Response;
 
 /**
  * A sample application that demonstrates setting up gRPC {@link Bridge} for
@@ -33,6 +51,23 @@ import spark.Request;
  */
 public class Application {
     private static final Logger logger = LoggerFactory.getLogger(Application.class);
+    private static final Map<Status.Code, Integer> errorMap =
+            new HashMap<Status.Code, Integer>() {
+                {
+                    put(INVALID_ARGUMENT, 400);
+                    put(FAILED_PRECONDITION, 400);
+                    put(OUT_OF_RANGE, 400);
+                    put(UNAUTHENTICATED, 401);
+                    put(PERMISSION_DENIED, 403);
+                    put(NOT_FOUND, 404);
+                    put(ALREADY_EXISTS, 409);
+                    put(RESOURCE_EXHAUSTED, 429);
+                    put(INTERNAL, 500);
+                    put(UNIMPLEMENTED, 501);
+                    put(UNAVAILABLE, 503);
+                    put(DEADLINE_EXCEEDED, 504);
+                }
+            };
 
     public static void main(String[] args) throws Exception {
         // Setup logging
@@ -54,7 +89,7 @@ public class Application {
                 .addService(proxyServer.bindService())
                 .build();
 
-        // Set port numnber
+        // Set port number
         port(config.getInt("port"));
 
         // Map Spark HTTP endpoints to the Bridge.
@@ -64,11 +99,15 @@ public class Application {
         delete("/*", (req, res) -> handle(bridge, req));
         patch("/*", (req, res) -> handle(bridge, req));
         exception(
-                BridgeException.class,
-                (error, req, res) -> logger.warn("Bridge error: " + error.getMessage()));
+                StatusRuntimeException.class,
+                Application::handleStatusError);
         exception(
-                RuntimeException.class,
-                (error, req, res) -> logger.error("Unhandled exception", error));
+                Exception.class,
+                (error, req, res) -> {
+                    logger.error("Unhandled exception", error);
+                    res.status(500);
+                    res.body(error.toString());
+                });
     }
 
     private static String handle(Bridge bridge, Request req) {
@@ -89,5 +128,19 @@ public class Application {
         context.getLogger("io.grpc.netty.NettyClientHandler").setLevel(Level.WARN);
         context.getLogger("io.netty").setLevel(Level.WARN);
         context.getLogger("io.token").setLevel(Level.WARN);
+    }
+
+    private static void handleStatusError(
+            Exception exception,
+            Request request,
+            Response response) {
+        logger.error("Status runtime exception: ", exception);
+        StatusRuntimeException ex = (StatusRuntimeException) exception;
+        Status status = ex.getStatus();
+        int code = errorMap.getOrDefault(status.getCode(), 500);
+        String error = Optional.ofNullable(status.getDescription()).orElse("Unknown");
+
+        response.status(code);
+        response.body(error);
     }
 }
